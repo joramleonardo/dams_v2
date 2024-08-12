d6c7cea8daf6b4772fb872abaff5dca580dd5b5a
joramleonardo <kateleonardo05@gmail.com>
2024-08-12 15:16:46 +0800

    updated router js

--- resources/js/router.js
+++ resources/js/router.js
@@ -5,50 +5,16 @@ import LoginPage from './views/Login.vue';
 
 //TICKETS
 import DAMS_Home from './views/DAMS-Home.vue';
-import Ticket_Home_Technical from './views/Ticket-Home-Technical.vue';
-import Ticket_Home_Request from './views/Ticket-Home-Request.vue';
-import Ticket_Home_FollowUp from './views/Ticket-Home-FollowUp.vue';
-import Ticket_Home_Feedback from './views/Ticket-Home-Feedback.vue';
-import Ticket_Rating from './views/Rating.vue';
+import DAMS_Categories from './views/DAMS-Categories.vue';
 
-import Ticket_Home_SuperAdmin from './views/Ticket-Home-SuperAdmin.vue';
-import Ticket_SuperAdmin_Dashboard from './views/Ticket-Admin-Dashboard.vue';
-import Ticket_SuperAdmin_Status from './views/Ticket-Status.vue';
-import Ticket_SuperAdmin_Pending from './views/Ticket-Pending.vue';
-import Ticket_SuperAdmin_Approved from './views/Ticket-Approved.vue';
-import Ticket_SuperAdmin_InProgress from './views/Ticket-InProgress.vue';
-import Ticket_SuperAdmin_Completed from './views/Ticket-Completed.vue';
-import Ticket_SuperAdmin_Closed from './views/Ticket-Closed.vue';
-import Ticket_SuperAdmin_Rescheduled from './views/Ticket-Rescheduled.vue';
+
+
+
+import Ticket_Home_Technical from './views/Ticket-Home-Technical.vue';
+
 
 import Ticket_Home_Admin from './views/Ticket-Home-Admin.vue';
-import Ticket_Admin_Dashboard from './views/Ticket-Admin-Dashboard.vue';
-import Ticket_Admin_Status from './views/Ticket-Status.vue';
-import Ticket_Admin_Pending from './views/Ticket-Pending.vue';
-import Ticket_Admin_Approved from './views/Ticket-Approved.vue';
-import Ticket_Admin_InProgress from './views/Ticket-InProgress.vue';
-import Ticket_Admin_Completed from './views/Ticket-Completed.vue';
-import Ticket_Admin_Closed from './views/Ticket-Closed.vue';
-import Ticket_Admin_Rescheduled from './views/Ticket-Rescheduled.vue';
 
-import Ticket_Home_Staff from './views/Ticket-Home-Staff.vue';
-import Ticket_Staff_Status from './views/Ticket-Status.vue';
-import Ticket_Staff_Pending from './views/Ticket-Pending.vue';
-import Ticket_Staff_Approved from './views/Ticket-Approved.vue';
-import Ticket_Staff_InProgress from './views/Ticket-InProgress.vue';
-import Ticket_Staff_Completed from './views/Ticket-Completed.vue';
-import Ticket_Staff_Closed from './views/Ticket-Closed.vue';
-import Ticket_Staff_Rescheduled from './views/Ticket-Rescheduled.vue';
-
-
-import Ticket_Home_Public from './views/Ticket-Public-Home.vue';
-import Ticket_Public_Dashboard from './views/Ticket-Public-Dashboard.vue';
-import Ticket_Public_Status from './views/Ticket-Public-Status.vue';
-import Ticket_Public_Completed from './views/Ticket-Public-Completed.vue';
-
-import Ticket_New_Employee from './views/New_Ticket_Employee.vue';
-import Ticket_New_STARBOOKS from './views/New_Ticket_starbooks.vue';
-import Ticket_New_Agency from './views/New_Ticket_OtherAgency.vue';
 
 
 import Admin_Dashboard from './views/Admin_Dashboard.vue';
---
+++ resources/js/views/DAMS-Categories.vue
@@ -0,0 +1,357 @@
+<template>
+    <div>
+        <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <a href="index.html" class="logo">
+                            <img src="img/logo-1.png" alt="SnapX Photography Template">
+                        </a>
+                        <ul class="nav">
+                            <li>
+                                <a class="">
+                                    <router-link to="/home" style="color: #fff !important;">
+                                        Home
+                                    </router-link>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="active">
+                                    <router-link to="/home" style="color: #fff !important;">
+                                        Categories
+                                    </router-link>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="">
+                                    <router-link to="/home" style="color: #fff !important;">
+                                        Photos
+                                    </router-link>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="">
+                                    <router-link to="/home" style="color: #fff !important;">
+                                        Videos
+                                    </router-link>
+                                </a>
+                            </li>
+                        </ul>   
+                        <!-- <div class="border-button">
+                            <a id="modal_trigger" href="#modal" class="sign-in-up"><i class="fa fa-user"></i> Sign In/Up</a>
+                        </div> -->
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        </header>
+        <!-- ***** Main Banner Area Start ***** -->
+        <div class="main-banner">
+            <div class="container">
+            <div class="row">
+                <div class="col-lg-12 ">
+                <div class="header-text">
+                    <h2><em>Digital Assets</em> Management System </h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id molestie lorem. Ut massa est, ornare et tristique a, fringilla quis purus. Vivamus elit ligula, accumsan vitae venenatis sed, fringilla at nisi.</p>
+                    
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <!-- ***** Main Banner Area End ***** -->
+
+
+        <section class="featured-items" id="featured-items">
+            <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                <div class="owl-features owl-carousel" style="position: relative; z-index: 5;">
+                    <div class="item">
+                    <div class="thumb">
+                        <img src="img/col/1.jpg" alt="">
+                        <div class="hover-effect">
+                        <div class="content">
+                            <h4>Walk In The Nature</h4>
+                            <ul>
+                            <li><span>Category:</span> Secretaries</li>
+                            <li><span>Photographer:</span> Anthony Soft</li>
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="item">
+                    <div class="thumb">
+                        <img src="img/col/2.jpg" alt="">
+                        <div class="hover-effect">
+                        <div class="content">
+                            <h4>Walk In The Nature</h4>
+                            <ul>
+                            <li><span>Category:</span> Secretaries</li>
+                            <li><span>Photographer:</span> Anthony Soft</li>
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="item">
+                    <div class="thumb">
+                        <img src="img/col/3.jpg" alt="">
+                        <div class="hover-effect">
+                        <div class="content">
+                            <h4>Walk In The Nature</h4>
+                            <ul>
+                            <li><span>Category:</span> Secretaries</li>
+                            <li><span>Photographer:</span> Anthony Soft</li>
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="item">
+                    <div class="thumb">
+                        <img src="img/col/4.jpg" alt="">
+                        <div class="hover-effect">
+                        <div class="content">
+                            <h4>Walk In The Nature</h4>
+                            <ul>
+                            <li><span>Category:</span> Secretaries</li>
+                            <li><span>Photographer:</span> Anthony Soft</li>
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="item">
+                    <div class="thumb">
+                        <img src="img/col/5.jpg" alt="">
+                        <div class="hover-effect">
+                        <div class="content">
+                            <h4>Walk In The Nature</h4>
+                            <ul>
+                            <li><span>Category:</span> Secretaries</li>
+                            <li><span>Photographer:</span> Anthony Soft</li>
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="item">
+                    <div class="thumb">
+                        <img src="img/col/6.jpg" alt="">
+                        <div class="hover-effect">
+                        <div class="content">
+                            <h4>Walk In The Nature</h4>
+                            <ul>
+                            <li><span>Category:</span> Secretaries</li>
+                            <li><span>Photographer:</span> Anthony Soft</li>
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="item">
+                    <div class="thumb">
+                        <img src="img/col/7.jpg" alt="">
+                        <div class="hover-effect">
+                        <div class="content">
+                            <h4>Walk In The Nature</h4>
+                            <ul>
+                            <li><span>Category:</span> Secretaries</li>
+                            <li><span>Photographer:</span> Anthony Soft</li>
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="item">
+                    <div class="thumb">
+                        <img src="img/col/8.jpg" alt="">
+                        <div class="hover-effect">
+                        <div class="content">
+                            <h4>Walk In The Nature</h4>
+                            <ul>
+                            <li><span>Category:</span> Secretaries</li>
+                            <li><span>Photographer:</span> Anthony Soft</li>
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="item">
+                    <div class="thumb">
+                        <img src="img/col/9.jpg" alt="">
+                        <div class="hover-effect">
+                        <div class="content">
+                            <h4>Walk In The Nature</h4>
+                            <ul>
+                            <li><span>Category:</span> Secretaries</li>
+                            <li><span>Photographer:</span> Anthony Soft</li>
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </section>
+
+
+        <section class="popular-categories">
+            <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                <div class="section-heading">
+                    <h4>Check Out the <em>Latest Events</em> this Month!</h4>
+                </div>
+                </div>
+                <div class="col-lg-6">
+                <div class="main-button">
+                    <a href="categories.html">Discover All Categories</a>
+                </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                <div class="popular-item">
+                    <div class="top-content">
+                    <div class="right">
+                        <h4>Nam finibus, diam at egestas semper</h4>
+                    </div>
+                    </div>
+                    <div class="thumb">
+                        <img src="img/col/1.jpg" alt="">
+                        <span class="likes"><i class="fa fa-heart"></i> 256</span>
+                    </div>
+                    <div class="border-button">
+                    <a href="contest-details.html">Browse Album</a>
+                    </div>
+                </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                <div class="popular-item">
+                    <div class="top-content">
+                    <div class="right">
+                        <h4>Duis iaculis purus eu odio fringilla</h4>
+                    </div>
+                    </div>
+                    <div class="thumb">
+                        <img src="img/col/2.jpg" alt="">
+                        <span class="likes"><i class="fa fa-heart"></i> 256</span>
+                    </div>
+                    <div class="border-button">
+                    <a href="contest-details.html">Browse Album</a>
+                    </div>
+                </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                <div class="popular-item">
+                    <div class="top-content">
+                    <div class="right">
+                        <h4>Etiam vehicula porta consequat</h4>
+                    </div>
+                    </div>
+                    <div class="thumb">
+                        <img src="img/col/3.jpg" alt="">
+                        <span class="likes"><i class="fa fa-heart"></i> 256</span>
+                    </div>
+                    <div class="border-button">
+                    <a href="contest-details.html">Browse Album</a>
+                    </div>
+                </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                <div class="popular-item">
+                    <div class="top-content">
+                    <div class="right">
+                        <h4>Duis maximus vulputate diam in convallis</h4>
+                    </div>
+                    </div>
+                    <div class="thumb">
+                        <img src="img/col/4.jpg" alt="">
+                        <span class="likes"><i class="fa fa-heart"></i> 256</span>
+                    </div>
+                    <div class="border-button">
+                    <a href="contest-details.html">Browse Album</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </section>
+
+        <section class="closed-contests">
+            <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                <div class="section-heading text-center">
+                    <h4>Watch our <em>Latest Coverage</em> on Youtube now</h4>
+                </div>
+                </div>
+                <div class="col-lg-12">
+                <div class="owl-features owl-carousel" style="position: relative; z-index: 5;">
+                    <div class="item">
+                        <div class="closed-item">
+                            <div class="thumb">
+                            <img src="img/col/7.jpg" alt="">
+                            </div>
+                            <div class="down-content">
+                            <div class="row">
+                                <div class="col-12">
+                                <h4>Aenean efficitur interdum orci at volutpa</h4>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="closed-item">
+                            <div class="thumb">
+                            <img src="img/col/8.jpg" alt="">
+                            </div>
+                            <div class="down-content">
+                            <div class="row">
+                                <div class="col-12">
+                                <h4>Aenean efficitur interdum orci at volutpa</h4>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="closed-item">
+                            <div class="thumb">
+                            <img src="img/col/9.jpg" alt="">
+                            </div>
+                            <div class="down-content">
+                            <div class="row">
+                                <div class="col-12">
+                                <h4>Aenean efficitur interdum orci at volutpa</h4>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-lg-12">
+                <div class="border-button text-center">
+                    <a href="contests.html">Visit us on Youtube</a>
+                </div>
+                </div>
+            </div>
+            </div>
+        </section>
+
+    </div>
+</template>
+
+<style>
+
+</style>
+
